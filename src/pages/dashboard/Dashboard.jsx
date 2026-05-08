252018f8491bfb12de3edc2c79cab2fe2f4675aa
Siitrev <paplata50@gmail.com>
2026-05-08 09:47:12 +0200

    Add files for basic structure

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Dashboard() {
+  return (
+    <div>
+      Dashboard
+    </div>
+  );
+}
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Profile() {
+  return (
+    <div>
+      Profile
+    </div>
+  );
+}
