513ea14d8c83e0bdd24b29e661a7dcbda0073724
Siitrev <paplata50@gmail.com>
2026-05-12 16:46:16 +0200

    Extract icons to components directory

---
+++ src/components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as MailIcon } from './MailIcon';
+export { default as LockIcon } from './LockIcon';
+export { default as EyeIcon } from './EyeIcon';
+export { default as EyeOffIcon } from './EyeOffIcon';
