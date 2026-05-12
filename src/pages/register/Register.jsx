5621378919c53fde0c71865b0a3bc15f9caba1dd
Siitrev <paplata50@gmail.com>
2026-05-12 21:54:31 +0200

    Remove unnecessary import

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, useParams, Link } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Register.css';
 import { EmailField, PasswordField, CheckboxField, TextField, PhoneField } from '../../components/fields';
 import { TermsLabel } from './TermsLabel';
